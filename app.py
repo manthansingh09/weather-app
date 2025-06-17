0bf91c79266b442b7579bebcf084c97695514c90
Manthan Singh <71974211+manthansingh09@users.noreply.github.com>
2025-06-17 23:17:03 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+API_KEY = 'eb754f1f98acad598a3eaae78382ce12'  
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    weather = {}
+    if request.method == 'POST':
+        city = request.form['city']
+        url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            weather = {
+                'city': city.title(),
+                'temperature': data['main']['temp'],
+                'description': data['weather'][0]['description'],
+                'icon': data['weather'][0]['icon'],
+                'humidity': data['main']['humidity'],
+                'wind': data['wind']['speed']
+            }
+        else:
+            weather = {'error': 'City not found!'}
+    return render_template('index.html', weather=weather)
+
+if __name__ == '__main__':
+    app.run(debug=True)
